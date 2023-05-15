cbaa057ca769a91b1a94d6b257afe4dc4fe5ffb9
Raphaël Castres <raphael.castres@epitech.eu>
2023-05-15 13:53:11 +0200

    ADD: link function to compile logic operator

--- asm/include/compil_line.h
+++ asm/include/compil_line.h
@@ -12,7 +12,12 @@
 
     typedef struct line_compil_s {
         char *command;
-        command_int_t (* compil) (prog_list_t *line);
+        command_int_t *(* compil) (prog_list_t *line, char **line_array,
+            int fnct_nbr);
+        int fnct_nbr;
     } line_compil_t;
+
+    command_int_t *compile_operator(prog_list_t *line, char **line_array,
+        int fnct_nbr);
 
 #endif /* !COMPIL_LINE_H_ */
