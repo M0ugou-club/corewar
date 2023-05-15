0b6b87d3e16b9fb8e9a04253e1368de7063c822c
zagreus <alberic.flour-de-saegher@epitech.eu>
2023-05-15 12:57:35 +0200

    ADD: get comment function and fill the structure header

--- asm/src/compil_header.c
+++ asm/src/compil_header.c
@@ -23,7 +23,7 @@ static int count_quote(const char *line)
     }
 }
 
-int get_name(file_t *file)
+int get_name(file_t *file, header_s *header)
 {
     if (file->line) {
         if (my_strncmp(file->line, ".name ", 6) != 0
@@ -39,19 +39,24 @@ int get_name(file_t *file)
         if (count_quote(file->line) != 0) {
             return SYNTAX_ERROR_STATUS;
         }
+        header->prog_name = file->line;
     }
     return 0;
 }
 
-int get_comment(file_t *file)
+int get_comment(file_t *file, header_s *header)
 {
     if (file && file->line) {
+        if (my_strncmp(file->line, ".comment ", 9) != 0) {
+            return COMMENT_ERROR_STATUS;
+        }
         if (my_strncmp(file->line, ".comment \"", 10) != 0) {
             return SYNTAX_ERROR_STATUS;
         }
         if (count_quote(file->line) != 0) {
             return SYNTAX_ERROR_STATUS;
         }
+        header->prog_comment = file->line;
     }
     return 0;
 }
@@ -61,21 +66,23 @@ int error_handling(int status)
     return 0;
 }
 
-int compil_header(file_t *file)
+int compil_header(file_t *file, header_s *header, int size)
 {
     file_t *temp = NULL;
     int status = 0;
 
-    if (file) {
+    if (file && header) {
         temp = file;
-        status = get_name(temp);
+        status = get_name(temp, header);
         status = error_handling(status);
         if (status == 84) {
             return 84;
         }
         temp = temp->next;
-        status = get_comment(temp);
+        status = get_comment(temp, header);
         status = error_handling(status);
+        header->magic = COREWAR_EXEC_MAGIC;
+        header->prog_size;
     }
     return status;
 }
