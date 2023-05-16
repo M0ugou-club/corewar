58e72cbee8c9d818ad60c2d5a8f0c22e01ba8c6a
Matthias SOUAL <matthias.soual@epitech.eu>
2023-05-16 12:13:35 +0200

    ADD: create_node function

--- corewar/memory/create_mem.c
+++ corewar/memory/create_mem.c
@@ -8,6 +8,20 @@
 #include <stdlib.h>
 #include "mem.h"
 
+static mem_t * create_node(const char data)
+{
+    mem_t *new_node = NULL;
+
+    new_node = malloc(sizeof(mem_t));
+    if (new_node == NULL) {
+        return (NULL);
+    }
+    new_node->oct = data;
+    new_node->prev = NULL;
+    new_node->next = NULL;
+    return (new_node);
+}
+
 mem_t *create_memory(unsigned const int mem_size) {
     mem_t *mem = NULL;
 
@@ -15,7 +29,11 @@ mem_t *create_memory(unsigned const int mem_size) {
         return (NULL);
     }
     mem = malloc(sizeof(mem_t));
+    if (mem ==  NULL) {
+        return (NULL);
+    }
     for (int i = 0; i < mem_size; i++) {
         
     }
+    return (mem);
 }
