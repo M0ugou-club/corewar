018ca492f38ef4ae82e60d04d2ce41afbf63ff51
Raphaël Castres <raphael.castres@epitech.eu>
2023-05-18 10:00:48 +0200

    FIX: make the include more precise

--- asm/include/compil_line.h
+++ asm/include/compil_line.h
@@ -16,14 +16,14 @@
         int fnct_nbr;
     } line_compil_t;
 
-    command_int_t *compile_operator(char **line_array, int fnct_nbr);
-    command_int_t *compile_calculation(char **line_array, int fnct_nbr);
-    command_int_t *compile_store(char **line_array, int fnct_nbr);
-    command_int_t *compile_store_index(char **line_array, int fnct_nbr);
-    command_int_t *compile_load(char **line_array, int fnct_nbr);
-    command_int_t *compile_load_index(char **line_array, int fnct_nbr);
-    command_int_t *compil_aff(char **line_array, int nb_ft);
+    command_int_t *compil_basic(char **line_array, int fnct_nbr);
+    command_int_t *compil_index(char **line_array, int fnct_nbr);
     command_int_t *compil_one_format_index(char **line_array, int nb_ft);
     command_int_t *compil_one_format(char **line_array, int nb_ft);
+    int get_nbr_value(char **value , int *value_size, char const *arg, int index);
+    int get_format_value(char **args);
+    int fill_multiple_args_line(command_int_t *command_int, char **line_array,
+        int fnct_nbr);
+    int change_size_index(command_int_t *command_int);
 
 #endif /* !COMPIL_LINE_H_ */
