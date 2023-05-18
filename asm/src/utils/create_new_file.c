388d5c0a3621e8c424f26aab301c5f39c2d46111
Raphaël Castres <raphael.castres@epitech.eu>
2023-05-18 15:41:26 +0200

    FIX: get the name of the file even if he is in an other folder

--- asm/src/utils/create_new_file.c
+++ asm/src/utils/create_new_file.c
@@ -9,9 +9,9 @@
 #include <stdlib.h>
 #include "my.h"
 
-static int get_index(char const *file_name)
+static int get_last_index(char const *file_name, int index_start)
 {
-    int i = 0;
+    int i = index_start;
 
     while (file_name[i] != '\0' && file_name[i] != '.'){
         i++;
@@ -19,19 +19,37 @@ static int get_index(char const *file_name)
     return (i);
 }
 
-static char *create_new_file_name(char const *file_name, int index)
+static int get_first_index(char const *file_name)
+{
+    int i = 0;
+
+    i = my_strlen(file_name);
+    while (i >= 0) {
+        if (file_name[i] == '/') {
+            return (i + 1);
+        }
+        i--;
+    }
+    if (i < 0) {
+        i = 0;
+    }
+    return (i);
+}
+
+static char *create_new_file_name(char const *file_name, int index_last,
+    int index_start)
 {
     static const char suffix[5] = ".cor";
     char *new_file_name = NULL;
     int len = 0;
 
-    len = index + my_strlen(suffix);
+    len = index_last - index_start + my_strlen(suffix);
     new_file_name = malloc(sizeof(char) * (len + 1));
     if (new_file_name == NULL) {
         return (NULL);
     }
     my_memset(new_file_name, '\0', len + 1);
-    my_strncpy(new_file_name, file_name, index);
+    my_strncpy(new_file_name, &file_name[index_start], (index_last - index_start));
     my_strcat(new_file_name, suffix);
     return (new_file_name);
 }
@@ -39,11 +57,13 @@ static char *create_new_file_name(char const *file_name, int index)
 int open_new_file(char const *file_name)
 {
     char *new_file = NULL;
-    int index = 0;
+    int index_first = 0;
+    int index_last = 0;
     int fd = 0;
 
-    index = get_index(file_name);
-    new_file = create_new_file_name(file_name, index);
+    index_first = get_first_index(file_name);
+    index_last = get_last_index(file_name, index_first);
+    new_file = create_new_file_name(file_name, index_last, index_first);
     if (new_file == NULL) {
         return (-1);
     }
