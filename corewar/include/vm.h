64139bf62766a1702dc3233373f298cd95005ab8
zagreus <alberic.flour-de-saegher@epitech.eu>
2023-05-19 13:27:20 +0200

    ADD: structure vm for stock usefull info

--- corewar/include/vm.h
+++ corewar/include/vm.h
@@ -7,5 +7,12 @@
 #ifndef VM_H_
     #define VM_H_
 
-int loop_process(process_t *process, char *memory);
+    #include "process.h"
+
+typedef struct vm_s {
+    char *memory;
+    int nb_alive;
+    int nb_champ;
+}vm_t;
+int loop_process(vm_t *vm, process_t *process);
 #endif /*VM_H_*/
