430e0fdc7af6c1f6af07b62695133a2bb3acc33e
zagreus <alberic.flour-de-saegher@epitech.eu>
2023-05-19 13:30:01 +0200

    ADD: take the structure vm in parmametre

--- corewar/src/vm.c
+++ corewar/src/vm.c
@@ -23,31 +23,32 @@ static int how_many_champ_alive(const process_t *process)
     return champ_alive;
 }
 
-static int incrementation_cycle(int *champ_alive, int cycle_to_die)
+static int incrementation_cycle(int cycle_to_die, vm_t *vm)
 {
-    *champ_alive = how_many_champ_alive(process);
-    if (*champ_alive > NBR_LIVE) {
+    if (vm->nb_alive > NBR_LIVE) {
         cycle_to_die -= CYCLE_DELTA;
     }
     return cycle_to_die;
 }
 
-int my_vm(char *memory, process_t *process)
+int my_vm(vm_t *vm, process_t *process)
 {
     int champ_alive = 0;
     int cycle_to_die = CYCLE_TO_DIE;
     int cycle = 0;
 
-    if (!memory || !process) {
+    if (!vm || !process) {
         return 84;
     }
     champ_alive = how_many_champ_alive(process);
     while (champ_alive > 1 && cycle_to_die > 0) {
-        loop_process(process, memory);
+        loop_process(vm, process);
         cycle++;
         if (cycle == cycle_to_die) {
-            cycle_to_die = incrementation_cycle(&champ_alive, cycle_to_die)
+            cycle_to_die = incrementation_cycle(cycle_to_die, vm)
             cycle = 0;
         }
+        champ_alive = how_many_champ_alive(process);
     }
+    return 0;
 }
