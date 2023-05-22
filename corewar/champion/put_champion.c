f157694ba509310fad862dfe632d6202f252022f
haihuac03 <hai-hua.chen@epitech.eu>
2023-05-22 13:25:04 +0200

    FIX: code organisation

--- corewar/champion/put_champion.c
+++ corewar/champion/put_champion.c
@@ -12,54 +12,6 @@
 #include "process.h"
 #include "vm.h"
 
-int count_champ(process_t *list_champ)
-{
-    int nb_champ = 0;
-    process_t *current = list_champ;
-
-    while (current) {
-        nb_champ += 1;
-        current = current->next;
-    }
-    return nb_champ;
-}
-
-int verify_size_champ(process_t *list_champ, int section_size)
-{
-    process_t *current = list_champ;
-    int size_of_code = 0;
-    while (current) {
-        size_of_code = strlen(current->code);
-        if (size_of_code > section_size) {
-            return -1;
-        }
-        current = current->next;
-    }
-    return 0;
-}
-
-void print_champ(process_t *liste)
-{
-    process_t *current = liste;
-    while (current != NULL) {
-        printf("ID: %s\n", current->id);
-        printf("Code: %s\n", current->code);
-        printf("\n");
-        current = current->next;
-    }
-}
-
-void free_champ(process_t *list_champ)
-{
-    process_t *current = list_champ;
-    process_t *next;
-    while (current) {
-        next = current->next;
-        free(current);
-        current = next;
-    }
-}
-
 int put_champ_in_arena(char *arena, int index, char *code)
 {
     for (int i = 0; code[i] != '\0' ;i++) {
@@ -89,7 +41,6 @@ char *put_champ(process_t *list_champ, vm_t *vm)
         index += MEM_SIZE / vm->nb_champ;
         current = current->next;
     }
-    write(1, vm->memory, MEM_SIZE);
     return NULL;
 }
 
@@ -116,24 +67,26 @@ process_t *add_champ(process_t *liste, char *id, char *code, int index)
     return liste;
 }
 
-int main(void)
-{
-    process_t *list_champ = NULL;
-    vm_t vm = {0};
-    char *arena = malloc(sizeof(char) * (MEM_SIZE + 1));
-     int longueur = MEM_SIZE;
-     for (int i = 0; i < longueur; i++) {
-         arena[i] = '\0';
-     }
-    arena[longueur] = '\0';
-    list_champ = add_champ(list_champ, "Champion 1", "Code du champion 1", -1);
-    list_champ = add_champ(list_champ, "Champion 2", "Code du champion 2", -1);
-    list_champ = add_champ(list_champ, "Champion 3", "Code du champion 3", -1);
-    list_champ = add_champ(list_champ, "Champion 4", "Code du champion 4", -1);
-    vm.memory = arena;
-    vm.nb_champ = count_champ(list_champ);
-    put_champ(list_champ, &vm);
-    free(arena);
-    free_champ(list_champ);
-    return 0;
-}
+// int main(void)
+// {
+//     process_t *list_champ = NULL;
+//     vm_t vm = {0};
+//     char *arena = malloc(sizeof(char) * (MEM_SIZE + 1));
+//     int longueur = MEM_SIZE;
+
+//     for (int i = 0; i < longueur; i++) {
+//         arena[i] = '-';
+//     }
+//     arena[longueur] = '\0';
+//     vm.memory = arena;
+//     list_champ = add_champ(list_champ, "Champion 1", "Code du champion 1", -1);
+//     list_champ = add_champ(list_champ, "Champion 2", "Code du champion 2", -1);
+//     list_champ = add_champ(list_champ, "Champion 3", "Code du champion 3", -1);
+//     list_champ = add_champ(list_champ, "Champion 4", "Code du champion 4", -1);
+//     vm.nb_champ = count_champ(list_champ);
+//     put_champ(list_champ, &vm);
+//     write(1, vm.memory, MEM_SIZE);
+//     free(arena);
+//     free_champ(list_champ);
+//     return 0;
+// }
