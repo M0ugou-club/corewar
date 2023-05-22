0ee87215b6613015e1a8b10aecc380f8e2690d2f
Matthias SOUAL <matthias.soual@epitech.eu>
2023-05-22 16:32:35 +0200

    ADD: create_process with get_header

--- corewar/src/param_parser.c
+++ corewar/src/param_parser.c
@@ -6,6 +6,7 @@
 */
 
 #include <stdlib.h>
+#include <fcntl.h>
 #include "my.h"
 #include "vm.h"
 
@@ -19,25 +20,41 @@ int is_opt(char **av, int i, process_t *process)
         process->nb_champ = my_atoi(av[i +1]);
         return (0);
     }
-    process->id = av[i];
     return -1;
 }
 
-process_t *parse_line(char **av)
+process_t *create_process(char *name, process_t *process)
+{
+    header_t *header = NULL;
+    int fd = 0;
+
+    fd = open(name, O_RDONLY);
+    if (fd < 0) {
+        return NULL;
+    }
+    header = get_header(fd);
+    process->id = header->prog_name;
+}
+
+process_t *param_parser(char **av)
 {
     int nb_champ = 1;
+    char *name = NULL;
     process_t *process = NULL;
+    process_t *head = NULL;
 
     process = malloc(sizeof(process_t));
     MALLOC_RETURN(process, NULL);
     for (int i = 1; av[i] != NULL; i++) {
         if (is_opt(av, i, process) == -1) {
-            process->next = malloc(sizeof(process_t));
-            MALLOC_RETURN(process->next, NULL);
-            process = process->next;
+            process = create_process(av[i], process);
+            process->nb_champ = nb_champ;
             nb_champ += 1;
+            head = add_process(process, head);
+            process = malloc(sizeof(process_t));
+            MALLOC_RETURN(process, NULL);
         }
         i += 1;
     }
-    return (process);
+    return (head);
 }
