7aa234061866c42986cf94fd7c5421e31d9c3051
Matthias SOUAL <matthias.soual@epitech.eu>
2023-05-22 16:43:49 +0200

    ADD: init_next_champ

--- corewar/src/param_parser.c
+++ corewar/src/param_parser.c
@@ -13,11 +13,11 @@
 int is_opt(char **av, int i, process_t *process)
 {
     if (my_strcmp(av[i], "-a") && av[i + 1] != NULL) {
-        process->index = my_atoi(av[i +1]);
+        process->index = my_atoi(av[i + 1]);
         return (0);
     }
     if (my_strcmp(av[i], "-n") && av[i + 1] != NULL) {
-        process->nb_champ = my_atoi(av[i +1]);
+        process->nb_champ = my_atoi(av[i + 1]);
         return (0);
     }
     return -1;
@@ -36,6 +36,16 @@ process_t *create_process(char *name, process_t *process)
     process->id = header->prog_name;
 }
 
+process_t *init_next_champ(char *champ_name, process_t *process,
+process_t *head)
+{
+    process = create_process(champ_name, process);
+    head = add_process(process, head);
+    process = malloc(sizeof(process_t));
+    MALLOC_RETURN(process, NULL);
+    process->nb_champ = 0;
+}
+
 process_t *param_parser(char **av)
 {
     int nb_champ = 1;
@@ -45,16 +55,17 @@ process_t *param_parser(char **av)
 
     process = malloc(sizeof(process_t));
     MALLOC_RETURN(process, NULL);
+    process->nb_champ = 0;
     for (int i = 1; av[i] != NULL; i++) {
-        if (is_opt(av, i, process) == -1) {
-            process = create_process(av[i], process);
+        if (process->nb_champ == 0) {
             process->nb_champ = nb_champ;
-            nb_champ += 1;
-            head = add_process(process, head);
-            process = malloc(sizeof(process_t));
-            MALLOC_RETURN(process, NULL);
         }
-        i += 1;
+        if (is_opt(av, i, process) == -1) {
+            nb_champ += 1;
+            process = init_next_champ(av[i], process, head);
+        } else {
+            i += 1;
+        }
     }
     return (head);
 }
