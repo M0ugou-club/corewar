848f1f90dd145e78734a7e94fd54d7d23900156c
Matthias SOUAL <matthias.soual@epitech.eu>
2023-05-22 15:53:19 +0200

    ADD: param_parser.c

--- corewar/src/param_parser.c
+++ corewar/src/param_parser.c
@@ -9,18 +9,35 @@
 #include "my.h"
 #include "vm.h"
 
-int get_dump_value(char **av)
+int is_opt(char **av, int i, process_t *process)
 {
-    int value = 0;
+    if (my_strcmp(av[i], "-a") && av[i + 1] != NULL) {
+        process->index = my_atoi(av[i +1]);
+        return (0);
+    }
+    if (my_strcmp(av[i], "-n") && av[i + 1] != NULL) {
+        process->nb_champ = my_atoi(av[i +1]);
+        return (0);
+    }
+    process->id = av[i];
+    return -1;
+}
 
-    for (int i = 0; av[i] != NULL; i++) {
-        if (my_strcmp(av[i], "-dump") && av[i + 1] != NULL) {
-            value = my_atoi(av[i +1]);
+process_t *parse_line(char **av)
+{
+    int nb_champ = 1;
+    process_t *process = NULL;
+
+    process = malloc(sizeof(process_t));
+    MALLOC_RETURN(process, NULL);
+    for (int i = 1; av[i] != NULL; i++) {
+        if (is_opt(av, i, process) == -1) {
+            process->next = malloc(sizeof(process_t));
+            MALLOC_RETURN(process->next, NULL);
+            process = process->next;
+            nb_champ += 1;
         }
+        i += 1;
     }
-    if (value < 0) {
-        return 0;
-    } else {
-        return value;
-    }
+    return (process);
 }
