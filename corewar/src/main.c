db5eaef5560a0622961c2766bb82f25ed4188aae
zagreus <alberic.flour-de-saegher@epitech.eu>
2023-05-23 15:24:50 +0200

    FIX: call of all function

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -11,18 +11,21 @@
 
 static int error_handling(int ac, char **argv)
 {
+    if (ac < 3) {
+        return 84;
+    }
     return 0;
 }
 
-vm_t *initialisation_of_vm(process_t *process, char **argv)
+vm_t *initialisation_of_vm(char **argv)
 {
     vm_t *vm = NULL;
 
     vm = malloc(sizeof(vm_t));
     if (vm) {
         vm->memory = create_memory();
-        vm->nb_champ = 0;
-        vm->nb_alive = vm->nb_champ;
+        vm->nb_champ = get_nb_champ(argv);
+        vm->nb_alive = 0;
         vm->f_dump = get_dump_value(argv);
     }
     return vm;
@@ -32,15 +35,18 @@ int main(int ac, char **argv)
 {
     process_t *process = NULL;
     vm_t *vm = NULL;
+    int return_value = 0;
 
     if (error_handling(ac, argv) != 0) {
         return 84;
     }
-    if (process) {
-        vm = initialisation_of_vm(process, argv);
-        if (vm) {
-            my_vm(vm, process);
+    vm = initialisation_of_vm(argv);
+    if (vm) {
+        process = param_parser(argv, vm);
+        if (process) {
+            return_value = my_vm(vm, process);
         }
     }
-    return 0;
+    free_champ(process);
+    return return_value;
 }
