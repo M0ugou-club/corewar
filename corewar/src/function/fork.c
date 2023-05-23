a45c55cacbbf2fa8cacece5c9e9064f5fc0b2bfd
Matthias SOUAL <matthias.soual@epitech.eu>
2023-05-23 17:16:44 +0200

    ADD: exec_fork

--- corewar/src/function/fork.c
+++ corewar/src/function/fork.c
@@ -7,34 +7,36 @@
 
 #include <stdlib.h>
 #include "fonction.h"
+#include "process.h"
 #include "vm.h"
+#include "op.h"
+#include "my.h"
 
-int process_fork(process_t *process, vm_t *vm, char **cb_tab)
+int process_fork(process_t *process, vm_t *vm)
 {
     int param = 0;
+    int new_index = 0;
+    process_t *new_process = NULL;
 
-    param = get_value(vm->memory, process->index + SKIP_COMM_CB,
-    cb_tab[INDEX_1ST]);
+    param = get_special_value(vm->memory, process->index + 1, T_IND, process);
+    if (param == -1) {
+        return (-1);
+    }
+    new_index = process->index + param % IDX_MOD;
+    new_process = create_process(new_index, my_strdup(process->id),
+    process->nb_champ);
+    process = add_process(new_process, process);
+    if (process == NULL) {
+        return (-1);
+    }
     return (0);
 }
 
 int exec_fork(process_t *process, vm_t *vm)
 {
-    char *cb_tab = NULL;
-    int index = 0;
-
-    cb_tab = get_coding_byte(vm->memory[process->index + 1]);
-    MALLOC_RETURN(cb_tab, -1);
-    if (get_type_error(cb_tab, vm->memory[process->index]) == -1) {
-        free(cb_tab);
+    if (process_fork(process, vm) == -1) {
         return (-1);
     }
-    process_fork(process, vm, cb_tab);
-    index = increase_index(cb_tab);
-    if (index == -1) {
-        free(cb_tab);
-        return (-1);
-    }
-    free(cb_tab);
-    return (process->index + index + SKIP_COMM_CB);
+    process->index += 2;
+    return 0;
 }
