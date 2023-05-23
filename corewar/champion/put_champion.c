f19ea3b335698dc92794b7d1fb90ecd3a198b337
Matthias SOUAL <matthias.soual@epitech.eu>
2023-05-23 10:25:12 +0200

    FIX: useless function and indentation

--- corewar/champion/put_champion.c
+++ corewar/champion/put_champion.c
@@ -15,22 +15,24 @@
 int put_champ_in_arena(char *arena, int index, char *code)
 {
     for (int i = 0; code[i] != '\0' ;i++) {
-        // if (get_mem_value(arena, index + 1) == 0) {
-        //     printf("ca ecrase \n");
-        //     return -1;
-        // }
+        if (get_mem_value(arena, index + 1) == 0) {
+            write(2, "champion overwrites another in memory\n", 38);
+            return -1;
+        }
         modif_mem(arena, index + i, code[i]);
     }
+    return (0);
 }
 
-char *put_champ(process_t *list_champ, vm_t *vm)
+int put_champ(process_t *list_champ, vm_t *vm)
 {
     int nb_champ = 0;
     int section_size = MEM_SIZE;
     int index = 0;
+    process_t *current = list_champ;
+
     nb_champ = count_champ(list_champ);
     section_size = (section_size / nb_champ);
-    process_t *current = list_champ;
     while (current) {
         if (current->index == -1) {
             put_champ_in_arena(vm->memory, index, current->code);
@@ -41,52 +43,5 @@ char *put_champ(process_t *list_champ, vm_t *vm)
         index += MEM_SIZE / vm->nb_champ;
         current = current->next;
     }
-    return NULL;
+    return 0;
 }
-
-process_t *add_champ(process_t *liste, char *id, char *code, int index)
-{
-    process_t *new_champ = malloc(sizeof(process_t));
-    if (new_champ == NULL) {
-        write(2, "error memory allocation\n", 25);
-        return NULL;
-    }
-    new_champ->id = id;
-    new_champ->code = code;
-    new_champ->index = index;
-    new_champ->next = NULL;
-    if (liste == NULL) {
-        return new_champ;
-    } else {
-        process_t *current = liste;
-        while (current->next != NULL) {
-            current = current->next;
-        }
-        current->next = new_champ;
-    }
-    return liste;
-}
-
-// int main(void)
-// {
-//     process_t *list_champ = NULL;
-//     vm_t vm = {0};
-//     char *arena = malloc(sizeof(char) * (MEM_SIZE + 1));
-//     int longueur = MEM_SIZE;
-
-//     for (int i = 0; i < longueur; i++) {
-//         arena[i] = '-';
-//     }
-//     arena[longueur] = '\0';
-//     vm.memory = arena;
-//     list_champ = add_champ(list_champ, "Champion 1", "Code du champion 1", -1);
-//     list_champ = add_champ(list_champ, "Champion 2", "Code du champion 2", -1);
-//     list_champ = add_champ(list_champ, "Champion 3", "Code du champion 3", -1);
-//     list_champ = add_champ(list_champ, "Champion 4", "Code du champion 4", -1);
-//     vm.nb_champ = count_champ(list_champ);
-//     put_champ(list_champ, &vm);
-//     write(1, vm.memory, MEM_SIZE);
-//     free(arena);
-//     free_champ(list_champ);
-//     return 0;
-// }
