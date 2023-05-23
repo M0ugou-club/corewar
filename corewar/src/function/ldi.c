9049448093f101c28c25e60edd7c7aa7c3009f05
zagreus <alberic.flour-de-saegher@epitech.eu>
2023-05-23 14:49:44 +0200

    ADD: function ldi

---
+++ corewar/src/function/ldi.c
@@ -0,0 +1,53 @@
+/*
+** EPITECH PROJECT, 2023
+** coreware
+** File description:
+** ldi.c
+*/
+
+#include <stdlib.h>
+#include "fonction.h"
+#include "vm.h"
+
+int process_ldi(process_t *process, vm_t *vm, char **cb_tab)
+{
+    int param1 = 0;
+    int param2 = 0;
+    int param3 = 0;
+    int result = 0;
+    int index = 0;
+
+    param1 = get_value(vm->memory, process->index + SKIP_COMM_CB,
+                       cb_tab[INDEX_1ST]);
+    index += get_index(cb_tab[INDEX_1ST]);
+    param2 = get_value(vm->memory, process->index + SKIP_COMM_CB +
+                                   index, cb_tab[INDEX_2ND]);
+    index += get_index(cb_tab[INDEX_2ND]);
+    param3 = get_value(vm->memory, process->index + SKIP_COMM_CB + index,
+                       cb_tab[INDEX_3RD]);
+    result = process->index + param1 % IDX_MOD;
+    result = get_value(vm->memory, result, IND_SIZ);
+    result += param2;
+    result = process->index + result % IDX_MOD;
+    process->register[param3] = result;
+}
+
+int exec_ldi(process_t *process, vm_t *vm) {
+    char *cb_tab = NULL;
+    int index = 0;
+
+    cb_tab = get_coding_byte(vm->memory[process->index + 1]);
+    MALLOC_RETURN(cb_tab, -1);
+    if (get_type_error(cb_tab, vm->memory[process->index]) == -1) {
+        free(cb_tab);
+        return (-1);
+    }
+    process_ldi(process, vm, cb_tab);
+    index = increase_index(cb_tab);
+    if (index == -1) {
+        free(cb_tab);
+        return (-1);
+    }
+    free(cb_tab);
+    return (process->index + index + SKIP_COMM_CB);
+}
