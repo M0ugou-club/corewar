af6f2abac28d69d847049477286504e0e4ec54c7
haihuac03 <hai-hua.chen@epitech.eu>
2023-05-23 14:22:51 +0200

    ADD: zjmp

---
+++ corewar/src/function/zjmp.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2023
+** corewar
+** File description:
+** zjmp
+*/
+
+#include <stdlib.h>
+#include "fonction.h"
+#include "process.h"
+#include "vm.h"
+
+int exec_zjmp(process_t *process, vm_t *vm)
+{
+    char *cb_tab = NULL;
+    int index = 0;
+
+    cb_tab = get_coding_byte(vm->memory[process->index + 1]);
+    MALLOC_RETURN(cb_tab, -1);
+
+    index = get_value(vm->memory, process->index + SKIP_COMM_CB, cb_tab[INDEX_1ST]);
+    if (index == -1) {
+        free(cb_tab);
+        return (-1);
+    }
+    free(cb_tab);
+    return (process->index + (index % IDX_MOD));
+}
