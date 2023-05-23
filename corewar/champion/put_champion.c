e1f0d9f5d5db0c58636b619b8cb6ad9127cd6d1f
Matthias SOUAL <matthias.soual@epitech.eu>
2023-05-23 11:05:10 +0200

    RM: no function

--- corewar/champion/put_champion.c
+++ corewar/champion/put_champion.c
@@ -24,24 +24,16 @@ int put_champ_in_arena(char *arena, int index, char *code)
     return (0);
 }
 
-int put_champ(process_t *list_champ, vm_t *vm)
+int put_champ(process_t *champ, vm_t *vm, char *code)
 {
-    int nb_champ = 0;
-    int section_size = MEM_SIZE;
-    int index = 0;
-    process_t *current = list_champ;
+    static int index = 0;
 
-    nb_champ = count_champ(list_champ);
-    section_size = (section_size / nb_champ);
-    while (current) {
-        if (current->index == -1) {
-            put_champ_in_arena(vm->memory, index, current->code);
-            current->index = index;
-        } else {
-            put_champ_in_arena(vm->memory, current->index, current->code);
-        }
-        index += MEM_SIZE / vm->nb_champ;
-        current = current->next;
+    if (champ->index == -1) {
+        put_champ_in_arena(vm->memory, index, code);
+        champ->index = index;
+    } else {
+        put_champ_in_arena(vm->memory, champ->index, code);
     }
+    index += MEM_SIZE / vm->nb_champ;
     return 0;
 }
