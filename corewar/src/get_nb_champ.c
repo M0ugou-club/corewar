fedd661ccf90d9226b73cbc0fcdd2e31eb5e76d1
Raphaël Castres <raphael.castres@epitech.eu>
2023-05-23 15:52:02 +0200

    FIX: merge two file

--- corewar/src/get_nb_champ.c
+++ corewar/src/get_nb_champ.c
@@ -7,6 +7,33 @@
 
 #include <stddef.h>
 #include <fcntl.h>
+#include "process.h"
+#include "vm.h"
+
+int is_champ_alive(process_t *process, int nb_champ)
+{
+    process_t *tmp = process;
+
+    while (tmp != NULL) {
+        if (my_strcmp(tmp->id, nb_champ) == 0 && tmp->index != -1) {
+            return (1);
+        }
+        tmp = tmp->next;
+    }
+    return (0);
+}
+
+int get_nb_champ_alive(process_t *process, vm_t *vm)
+{
+    int nb_loop = vm->nb_champ;
+    process_t *tmp = process;
+    int alive = 0;
+
+    for (int i = 0; i != nb_loop; i++, tmp = tmp->next) {
+        alive += is_champ_alive(process, tmp->nb_champ);
+    }
+    return (alive);
+}
 
 int get_nb_champ(char **argv)
 {
