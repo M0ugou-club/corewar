c9e95aadc9bad08ec5e73bda40ff6a2cffcaeb72
Raphaël Castres <raphael.castres@epitech.eu>
2023-05-23 10:52:56 +0200

    FIX: error handling

--- corewar/src/param_parser.c
+++ corewar/src/param_parser.c
@@ -18,7 +18,7 @@ int is_opt(char **av, int i, process_t *process)
     if (my_strcmp(av[i], "-a") && av[i + 1] != NULL) {
         if (my_str_is_num(av[1]) == -1) {
             write(2, ERROR, sizeof(ERROR));
-            exit(84);
+            return (-1);
         }
         process->index = my_atoi(av[i + 1]);
         return (0);
@@ -26,12 +26,12 @@ int is_opt(char **av, int i, process_t *process)
     if (my_strcmp(av[i], "-n") && av[i + 1] != NULL) {
         if (my_str_is_num(av[1]) == -1) {
             write(2, ERROR, sizeof(ERROR));
-            exit(84);
+            return (-1);
         }
         process->nb_champ = my_atoi(av[i + 1]);
         return (0);
     }
-    return -1;
+    return 1;
 }
 
 process_t *create_process(char *name, process_t *process)
@@ -61,9 +61,30 @@ process_t *head)
     return process;
 }
 
+int test_parser(process_t *process, process_t *head, int i, char **av)
+{
+    static int nb_champ = 1;
+    int val_ret = 0;
+
+    if (process->nb_champ == 0) {
+        process->nb_champ = nb_champ;
+    }
+    val_ret = is_opt(av, i, process);
+    if (val_ret == -1) {
+        return (-1);
+    }
+    if (val_ret == 1) {
+        nb_champ += 1;
+        process = init_next_champ(av[i], process, head);
+        MALLOC_RETURN(process, -1);
+    } else {
+        i++;
+    }
+    return (i);
+}
+
 process_t *param_parser(char **av)
 {
-    int nb_champ = 1;
     process_t *process = NULL;
     process_t *head = NULL;
 
@@ -71,14 +92,9 @@ process_t *param_parser(char **av)
     MALLOC_RETURN(process, NULL);
     process->nb_champ = 0;
     for (int i = 1; av[i] != NULL; i++) {
-        if (process->nb_champ == 0) {
-            process->nb_champ = nb_champ;
-        }
-        if (is_opt(av, i, process) == -1) {
-            nb_champ += 1;
-            process = init_next_champ(av[i], process, head);
-        } else {
-            i += 1;
+        i = test_parser(process, head, i, av);
+        if (i == -1) {
+            return (NULL);
         }
     }
     return (head);
