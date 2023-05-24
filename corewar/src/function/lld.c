79bba07b46f442b58ec1878a9f264f8e080382b5
haihuac03 <hai-hua.chen@epitech.eu>
2023-05-24 11:06:29 +0200

    FIX: load instruction

--- corewar/src/function/lld.c
+++ corewar/src/function/lld.c
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2023
 ** corewar
 ** File description:
-** load
+** load long
 */
 
 #include <stdlib.h>
@@ -10,36 +10,35 @@
 #include "process.h"
 #include "vm.h"
 
-int load_value(process_t *process, vm_t *vm, char coding_byte, int val2)
+int lld_value(process_t *process, vm_t *vm, char coding_byte, int val2)
 {
     int val1 = 0;
 
     val1 = get_value(vm->memory, process->index + SKIP_COMM_CB,
     coding_byte);
     if (coding_byte == T_IND) {
-        val1 = get_mem_value(process->index + (val1 % IDX_MOD));
+        val1 = get_mem_value(process->index + val1);
     }
     process->registers[val2 - 1] = val1;
     return 0;
 }
 
-int process_load_value(process_t *process, vm_t *vm, char *cb_tab)
+int process_lld(process_t *process, vm_t *vm, char *cb_tab)
 {
+    int val1 = 0;
     int reg2 = 0;
-    int index = 0;
 
-    index = get_index_arg(cb_tab[INDEX_2ND], false);
-    reg2 = get_value(vm->memory, process->index + SKIP_COMM_CB + index,
+    reg2 = get_value(vm->memory, process->index + SKIP_COMM_CB,
     cb_tab[INDEX_2ST]);
     if (get_reg_error(reg2) == -1) {
         return (-1);
     }
-    if (load_value(process, vm, cb_tab[INDEX_1ND], reg2) == -1)
+    if (lld_value(process, vm, cb_tab[INDEX_1ND], reg2) == -1)
         return -1;
     return (0);
 }
 
-int exec_ld(process_t *process, vm_t *vm)
+int exec_st(process_t *process, vm_t *vm)
 {
     char *cb_tab = NULL;
     int index = 0;
@@ -50,7 +49,7 @@ int exec_ld(process_t *process, vm_t *vm)
         free(cb_tab);
         return (-1);
     }
-    if (process_load_value(process, vm, cb_tab) == -1) {
+    if (process_lld(process, vm, cb_tab) == -1) {
         free(cb_tab);
         return -1;
     }
