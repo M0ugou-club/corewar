722c5d87100b3e9f295ff8ffd579d0826bec620d
Raphaël Castres <raphael.castres@epitech.eu>
2023-05-24 19:29:01 +0200

    FIX: free the memeory in fork and return value

--- corewar/src/function/fork.c
+++ corewar/src/function/fork.c
@@ -17,31 +17,38 @@ int process_fork(process_t *process, vm_t *vm)
 {
     int param = 0;
     int new_index = 0;
+    char *copy_name = NULL;
     process_t *new_process = NULL;
 
-    param = get_special_value(vm->memory, process->index + 1, T_IND, process);
-    if (param == -1) {
-        return (-1);
-    }
+    param = get_value(vm->memory, process->index + 1, T_IND);
     if (get_mem_value(vm->memory, process->index) == 12) {
-        new_index = process->index + param % IDX_MOD;
+        new_index = circular_mod(process->index + param % IDX_MOD);
     } else {
-        new_index = process->index + param;
+        new_index = circular_mod(process->index + param);
     }
-    new_process = create_process(new_index, my_strdup(process->id),
-    process->nb_champ);
-    process = add_process(new_process, process);
-    if (process == NULL) {
+    copy_name = my_strdup(process->id);
+    MALLOC_RETURN(copy_name, -1);
+    new_process = copy_process(new_index, copy_name, process->nb_champ);
+    if (new_process == NULL) {
+        free(copy_name);
         return (-1);
     }
+    process = add_process(new_process, process);
     return (0);
 }
 
 int exec_fork(process_t *process, vm_t *vm)
 {
-    if (process_fork(process, vm) == -1) {
+    int ret_val = 0;
+
+    ret_val = process_fork(process, vm);
+    if (ret_val == -1) {
         return (-1);
     }
-    process->index += 2;
+    if (ret_val != 0) {
+        process->index = -1;
+        return (0);
+    }
+    process->index += 3;
     return 0;
 }
